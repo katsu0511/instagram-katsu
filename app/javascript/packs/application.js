38fb9147d8f8043f7843c8c5f0c191af900a38fd
Katsuya <haradakatsuya190511@gmail.com>
2020-08-17 13:45:56 +0900

    jQueryを導入

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -15,3 +15,11 @@ require("channels")
 //
 // const images = require.context('../images', true)
 // const imagePath = (name) => images(name, true)
+
+import $ from 'jquery'
+
+document.addEventListener('DOMContentLoaded', () => {
+  $('h1').on('click', () => {
+    window.alert('CLICKED')
+  })
+})
