af6cc15bf439341a83accb152cacef1174ceda35
Katsuya <haradakatsuya190511@gmail.com>
2020-08-17 21:10:16 +0900

    axiosのファイル切り分け

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -15,7 +15,4 @@ require("channels")
 // const imagePath = (name) => images(name, true)
 
 import $ from 'jquery'
-import axios from 'axios'
-import { csrfToken } from 'rails-ujs'
-
-axios.defaults.headers.common['X-CSRF-Token'] = csrfToken()
+import axios from 'modules/axios'
