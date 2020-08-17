bf9af44671d339ad49836899b21218ce23b5a53f
Katsuya <haradakatsuya190511@gmail.com>
2020-08-17 21:53:11 +0900

    iconを変えられるようにした

--- app/controllers/profiles_controller.rb
+++ app/controllers/profiles_controller.rb
@@ -7,4 +7,20 @@ class ProfilesController < ApplicationController
   def edit
     @profile = current_user.prepare_profile
   end
+
+  def update
+    @profile = current_user.prepare_profile
+    @profile.assign_attributes(profile_params)
+    if @profile.save
+      redirect_to profile_path, notice: 'Successfully updated profile!'
+    else
+      flash.now[:error] = 'Failed to update profile'
+      render :edit
+    end
+  end
+
+  private
+  def profile_params
+    params.require(:profile).permit(:icon)
+  end
 end
