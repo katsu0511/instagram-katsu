e9afc8201b86bdbbbb54e82abea9fbcd5c80f73e
Katsuya <haradakatsuya190511@gmail.com>
2020-08-17 14:37:04 +0900

    profile.jsに分割

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -21,12 +21,3 @@ import axios from 'axios'
 import { csrfToken } from 'rails-ujs'
 
 axios.defaults.headers.common['X-CSRF-Token'] = csrfToken()
-
-document.addEventListener('turbolinks:load', () => {
-  $('h1').on('click', () => {
-    axios.get('/')
-      .then((response) => {
-        console.log(response)
-      })
-  })
-})
---
+++ app/javascript/packs/profile.js
@@ -0,0 +1,14 @@
+import $ from 'jquery'
+import axios from 'axios'
+import { csrfToken } from 'rails-ujs'
+
+axios.defaults.headers.common['X-CSRF-Token'] = csrfToken()
+
+document.addEventListener('turbolinks:load', () => {
+  $('h1').on('click', () => {
+    axios.get('/')
+      .then((response) => {
+        console.log(response)
+      })
+  })
+})
