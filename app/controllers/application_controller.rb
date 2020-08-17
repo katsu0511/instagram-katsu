947c783e7278f89cbc202fa7ed17d8b1e0fb8c3c
Katsuya <haradakatsuya190511@gmail.com>
2020-08-17 09:00:30 +0900

    ログインしていないと入れないようにした

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,10 @@
 class ApplicationController < ActionController::Base
+  before_action :forbid_logout_user
+
+  private
+  def forbid_logout_user
+    if !signed_in?
+      redirect_to new_user_session_path, notice: 'You need to sign in'
+    end
+  end
 end
