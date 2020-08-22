8ab4db72070ec04eaa78743810cdffda50fbcaf8
Katsuya <haradakatsuya190511@gmail.com>
2020-08-22 14:16:04 +0900

    コメントできるようにした

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,24 +1,29 @@
 class CommentsController < ApplicationController
   def index
     post = Post.find(params[:post_id])
-    @comments = post.comments.all
-  end
-
-  def show
+    @comments = post.comments
   end
 
   def new
+    post = Post.find(params[:post_id])
+    @comment = post.comments.build
   end
 
   def create
+    post = Post.find(params[:post_id])
+    @comment = post.comments.build(comment_params)
+    @comment.user_id = current_user.id
+
+    if @comment.save
+      redirect_to post_comments_path(post), notice: 'Successfully posted a comment!'
+    else
+      flash.now[:error] = 'Failed to post'
+      render :new
+    end
   end
 
-  def edit
-  end
-
-  def update
-  end
-
-  def destroy
+  private
+  def comment_params
+    params.require(:comment).permit(:content)
   end
 end
