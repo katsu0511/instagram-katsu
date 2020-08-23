fb848a85bea5657d1790c86d0b8e6915c263c2a9
Katsuya <haradakatsuya190511@gmail.com>
2020-08-23 23:40:46 +0900

    アンフォロー機能

--- app/models/user.rb
+++ app/models/user.rb
@@ -45,17 +45,13 @@ class User < ApplicationRecord
   end
 
   def follow!(user)
-    if user.is_a?(User)
-      user_id = user.id
-    else
-      user_id = user
-    end
-
+    user_id = get_user_id(user)
     following_relationships.create!(following_id: user_id)
   end
 
   def unfollow!(user)
-    relation = following_relationships.find_by!(following_id: user.id)
+    user_id = get_user_id(user)
+    relation = following_relationships.find_by!(following_id: user_id)
     relation.destroy!
   end
 
@@ -68,6 +64,15 @@ class User < ApplicationRecord
       profile.icon
     else
       'default'
+    end
+  end
+
+  private
+  def get_user_id(user)
+    if user.is_a?(User)
+      user.id
+    else
+      user
     end
   end
 end
