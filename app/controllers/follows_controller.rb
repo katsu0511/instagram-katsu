2d9955043e71901eb2a32be00153610f496cdcc4
Katsuya <haradakatsuya190511@gmail.com>
2020-08-25 10:21:17 +0900

    ajaxでfollowの状態

--- app/controllers/follows_controller.rb
+++ app/controllers/follows_controller.rb
@@ -9,11 +9,13 @@ class FollowsController < ApplicationController
 
   def create
     current_user.follow!(params[:account_id])
-    redirect_to account_path(params[:account_id])
+
+    render json: { status: 'ok' }
   end
 
   def destroy
     current_user.unfollow!(params[:account_id])
-    redirect_to account_path(params[:account_id])
+
+    render json: { status: 'ok' }
   end
 end
