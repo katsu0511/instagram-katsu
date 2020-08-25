123343b34e7419aa349a3faa1e2f249b0b26a6f7
Katsuya <haradakatsuya190511@gmail.com>
2020-08-25 09:36:01 +0900

    accountId

--- app/javascript/packs/profile.js
+++ app/javascript/packs/profile.js
@@ -2,5 +2,11 @@ import $ from 'jquery'
 import axios from 'modules/axios'
 
 document.addEventListener('DOMContentLoaded', () => {
-  
+  const dataset = $('#account-show').data()
+  const accountId = dataset.accountId
+
+  axios.get(`/accounts/${accountId}/follow`)
+    .then((response) => {
+      console.log(response)
+    })
 })
