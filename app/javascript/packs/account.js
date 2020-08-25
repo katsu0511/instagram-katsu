e6fdd75750e0e61fa4659869663307e36ced56e5
Katsuya <haradakatsuya190511@gmail.com>
2020-08-25 09:47:47 +0900

    account.js

--- app/javascript/packs/account.js
+++ app/javascript/packs/account.js
@@ -1,3 +1,17 @@
 import $ from 'jquery'
 import axios from 'modules/axios'
 
+document.addEventListener('DOMContentLoaded', () => {
+  const dataset = $('#account-show').data()
+  const accountId = dataset.accountId
+
+  axios.get(`/accounts/${accountId}/follow`)
+    .then((response) => {
+      const hasFollowed = response.data.hasFollowed
+      if (hasFollowed) {
+        $('.unfollow-btn').removeClass('hidden')
+      } else {
+        $('.follow-btn').removeClass('hidden')
+      }
+    })
+})
