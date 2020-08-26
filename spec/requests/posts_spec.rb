0fa2a9e2c9f6f272a03ab760232b0d55b4f8975d
Katsuya <haradakatsuya190511@gmail.com>
2020-08-26 17:00:37 +0900

    テスト追加

--- spec/requests/posts_spec.rb
+++ spec/requests/posts_spec.rb
@@ -1,8 +1,11 @@
 require 'rails_helper'
 
 RSpec.describe 'Posts', type: :request do
+  let!(:user) { create(:user) }
+  let!(:posts) { create_list(:post, 3, user: user) }
+
   describe 'GET /posts' do
-    it '200ステータスが帰ってくる' do
+    it '200ステータスが返ってくる' do
       get posts_path
       expect(response).to have_http_status(200)
     end
