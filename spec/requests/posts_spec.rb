f9232cc74df2ad20f9191f6b61ce74bfb8639a10
Katsuya <haradakatsuya190511@gmail.com>
2020-08-26 17:12:23 +0900

    post リクエストのテスト

--- spec/requests/posts_spec.rb
+++ spec/requests/posts_spec.rb
@@ -4,10 +4,18 @@ RSpec.describe 'Posts', type: :request do
   let!(:user) { create(:user) }
   let!(:posts) { create_list(:post, 3, user: user) }
 
-  describe 'GET /posts' do
-    it '200ステータスが返ってくる' do
-      get posts_path
-      expect(response).to have_http_status(200)
+  describe 'POST /posts' do
+    context 'ログインしている場合' do
+      before do
+        sign_in user
+      end
+
+      it '記事が保存される' do
+        post_params = attributes_for(:post)
+        post posts_path({post: post_params})
+        expect(response).to have_http_status(302)
+        expect(Post.last.content).to eq(post_params[:content])
+      end
     end
   end
 end
