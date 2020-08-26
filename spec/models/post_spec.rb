025c8a1c9c2e21fe68261d963c8bffa935d40493
Katsuya <haradakatsuya190511@gmail.com>
2020-08-26 14:05:42 +0900

    let

--- spec/models/post_spec.rb
+++ spec/models/post_spec.rb
@@ -2,18 +2,21 @@ require 'rails_helper'
 
 RSpec.describe Post, type: :model do
   context '内容が入力されている場合' do
-    before do
-      user = User.create!({
+    let!(:user) do
+      User.create!({
         email: 'test@example.com',
         password: 'password'
       })
-      @post = user.posts.build({
+    end
+
+    let!(:post) do
+      user.posts.build({
         content: Faker::Lorem.characters(number: 140)
       })
     end
 
     it '投稿を保存できる' do
-      expect(@post).to be_valid
+      expect(post).to be_valid
     end
   end
 end
