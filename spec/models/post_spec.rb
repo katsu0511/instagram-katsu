06b6d84240262098a738688b671c12a38ec44f73
Katsuya <haradakatsuya190511@gmail.com>
2020-08-26 13:56:52 +0900

    モデルのテストを書いてみた

--- spec/models/post_spec.rb
+++ spec/models/post_spec.rb
@@ -1,4 +1,15 @@
 require 'rails_helper'
 
 RSpec.describe Post, type: :model do
+  it '内容が入力されていれば、投稿を保存できる' do
+    user = User.create!({
+      email: 'test@example.com',
+      password: 'password'
+    })
+    post = user.posts.build({
+      content: Faker::Lorem.characters(number: 140)
+    })
+
+    expect(post).to be_valid
+  end
 end
