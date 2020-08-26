52441c7817fb00579a2fecaa8e9ecca2e2cc19db
Katsuya <haradakatsuya190511@gmail.com>
2020-08-26 14:30:33 +0900

    post factoryを登録

--- spec/models/post_spec.rb
+++ spec/models/post_spec.rb
@@ -4,11 +4,7 @@ RSpec.describe Post, type: :model do
   let!(:user) { create(:user) }
 
   context '内容が入力されている場合' do
-    let!(:post) do
-      user.posts.build({
-        content: Faker::Lorem.characters(number: 140)
-      })
-    end
+    let!(:post) { build(:post, user: user) }
 
     it '投稿を保存できる' do
       expect(post).to be_valid
@@ -16,10 +12,10 @@ RSpec.describe Post, type: :model do
   end
 
   context '内容の文字が一文字の場合' do
-    let!(:post) do
-      user.posts.create({
-        content: Faker::Lorem.characters(number: 1)
-      })
+    let!(:post) { build(:post, content: Faker::Lorem.characters(number: 1), user: user) }
+
+    before do
+      post.save
     end
 
     it '記事を保存できない' do
